03fc343da22c0a18f71e0b7b07148b7bbed7351f
TStavale <tassiostavale@hotmail.com>
2022-12-05 22:56:47 -0300

    Estudando Biblioteca Math

---
+++ JavaScript/Biblioteca Math/script.js
@@ -0,0 +1,10 @@
+/* MATH */
+
+// Alguns exemplos do uso do Math
+
+console.log(Math.PI) // Mostra o Valor de PI.
+console.log(Math.pow(2,3)) // Permite usarmos potenciação.
+console.log(Math.sqrt(9,2)) // Permite calcularmos a raiz de um número.
+console.log(Math.random()) // Mostra um número aleatório entre 0 e 1.
+console.log(Math.ceil(23.6)) // Arredonda um número para CIMA.
+console.log(Math.floor(23.6)) // Arredonda um número para BAIXO.
