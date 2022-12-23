3812e7408e797aee59238230f6a46711e40ad661
TStavale <tassiostavale@hotmail.com>
2022-12-23 17:40:43 -0300

    Concluindo alguns exercícios de javaScript.

--- JavaScript/Js/Exercicio - Js03/script.js
+++ JavaScript/Js/Exercicio - Js03/script.js
@@ -142,12 +142,152 @@ function findAverageAndHighestSalary (populationInformation) {
 
 findAverageAndHighestSalary(population);
 
-// Exercício 07 - Faça um programa onde, você chame uma função que calcule a taxa mensal de juros de um investimento.
-
-
-
-/* Exercício 08 - Você deve enviar como argumentos da função - Valor inicial investido - Valor atual do investimento - 
-Tempo em meses, que o valor está investidoA função deve RETORNAR a taxa de juros, já formatada. Ex: 2,5%. 
+/* Exercício 07 - Faça um programa onde, você chame uma função que calcule a taxa mensal de juros de um investimento.
+ - Você deve enviar como argumentos da função 
+    - Valor inicial investido 
+    - Valor atual do investimento 
+    - Tempo em meses, que o valor está investido. 
+- A função deve RETORNAR a taxa de juros, já formatada. Ex: 2,5%. 
 Siga a formula desse vídeo para te auxiliar com o cálculo.
 
 Cálculo taxa = juros(Valor atual do investimento - Valor inicial investido) / Valor inicial investido * tempo. */
+
+function monthlyFeeCalculation (initialValue, currentValue, time) {
+
+    const juros = (currentValue - initialValue) / (initialValue * time);
+
+    console.log("Resposta do exercício 07: " + (juros * 100).toFixed(2) + "%.");
+}
+
+monthlyFeeCalculation(700, 1100, 12);
+
+/* Exercício 08 - Escreva um programa onde, você cria uma função geradora de desconto.
+    - A função aceita 4 parâmetros (Nome do cliente, valor total da compra, um booleano que diz se é a primeira compra do cliente ou não, um booleando que diz se o cliente vai pagar a vista ou não)
+    - Os descontos funcionam da seguinte forma:
+        - Se é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 30% de desconto
+        - Se é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 25% de desconto
+        - Se é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 20%
+        - Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto 
+        - Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto
+        - Se é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => desconto de 10%
+        - Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto
+        - Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto
+        - Se NÃO é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 10%
+        - Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 10% de desconto 
+        - Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 5% de desconto
+        - Se NÃO é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => SEM DESCONTO 
+        - A função deve imprimir na tela:
+        - Se o cliente recebeu desconto, imprima na tela um agradecimento pela compra, o valor total sem desconto, o valor total com desconto e a porcentagem de desconto que ele recebeu.
+        - Se o cliente não recebeu desconto, imprima na tela um agradecimento pela compra, o valor total da sua compra e um cupom de desconto. Esse cupom deve ser um valor aleatório entre 10 e 20% para a próxima compra. */
+
+function discountCalculation (name, value, isFirstPurchase, isCashPayment) {
+
+    if(isFirstPurchase && isCashPayment) {
+
+        if(value > 1000) {
+        
+            const discounted = (value * 30) / 100;
+            const discountedValue = value - discounted;
+
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 30%.`);
+        }
+        else if (value > 500) {
+
+            const discounted = (value * 25) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 25%.`);
+        }
+        else {
+
+            const discounted = (value * 20) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 20%.`);
+        
+        }
+    }
+
+    if(isFirstPurchase && !isCashPayment) {
+
+        if(value > 1000) {
+
+            const discounted = (value * 20) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 20%.`);
+        
+        }
+        else if (value > 500) {
+
+            const discounted = (value * 15) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 15%.`);
+        
+        }
+        else {
+
+            const discounted = (value * 10) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 10%.`);
+        
+        }
+    }
+
+    if(!isFirstPurchase && isCashPayment) {
+
+        if(value > 1000) {
+
+            const discounted = (value * 20) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 20%.`);
+        
+        }
+        else if (value > 500) {
+
+            const discounted = (value * 15) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 15%.`);
+        
+        }
+        else {
+
+            const discounted = (value * 10) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 10%.`);
+        
+        }
+    }
+
+    if(!isFirstPurchase && !isCashPayment) {
+
+        if(value > 1000) {
+            
+            const discounted = (value * 10) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 10%.`);
+        
+        }
+        else if (value > 500) {
+            
+            const discounted = (value * 5) / 100;
+            const discountedValue = value - discounted;
+            
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, o valor com desconto foi de R$${discountedValue} e o desconto aplicado foi de 5%.`);
+        
+        }
+        else {
+            const randomDiscount = (Math.random() * (20 -10) + 10).toFixed(0);
+            console.log(`Resposta do exercício 08: Obrigado Sr(a). ${name} por comprar com a gente, o total da sua compra sem desconto foi de R$${value}, receba um cupom de ${randomDiscount}% de descontro para a proxima compra.`);
+        }
+    }
+
+}
+
+discountCalculation("Julia", 800, true, false);
